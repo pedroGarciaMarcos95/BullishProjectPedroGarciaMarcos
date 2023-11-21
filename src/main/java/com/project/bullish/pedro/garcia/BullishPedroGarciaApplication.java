80b5d576479a2c339529be3ffa42896c34ebe72e
p-garcia <p.garcia@aspanetconomy.net>
2023-11-21 16:47:57 +0100

    Bullish first commit

---
+++ src/main/java/com/project/bullish/pedro/garcia/BullishPedroGarciaApplication.java
@@ -0,0 +1,13 @@
+package com.project.bullish.pedro.garcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullishPedroGarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullishPedroGarciaApplication.class, args);
+	}
+
+}
